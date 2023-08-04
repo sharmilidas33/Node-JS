65b7ed3a533b11405c796accc15df95fc76f856e
Sharmili Das <128738858+sharmilidas33@users.noreply.github.com>
2023-08-04 20:09:32 +0530

    Add files via upload

---
+++ ES6 Modules in JS/modules3.js
@@ -0,0 +1,11 @@
+//ES6 Modules.
+export const greet =()=>{
+    console.log("Good morning");
+}
+export const Agreet=(name)=>{
+    console.log("Good morning "+name);
+}
+const hello= ()=>{
+    console.log("Hello World");
+}
+export default hello;
---
+++ ES6 Modules in JS/modules4.js
@@ -0,0 +1,7 @@
+import hello, { greet, Agreet} from "./modules3.js";
+//You have to add ["type": "module"] in package.json.
+greet();
+Agreet("Sharmili");
+Agreet("Titli");
+
+hello();
